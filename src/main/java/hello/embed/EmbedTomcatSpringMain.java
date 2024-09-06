eb1d376a8358790a9912e42a974bf3c3c6471123
johnny-gill <uint777@gmail.com>
2024-09-06 10:56:29 +0900

    내장톰캣에 스프링 추가

---
+++ src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -0,0 +1,35 @@
+package hello.embed;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EmbedTomcatSpringMain {
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatSpringMain.main");
+
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+
+        // 1. Spring Container를 만들고 Bean을 등록
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        // 2. DispatcherServlet를 만들고 Spring Container를 등록
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        // 3. DispatcherServlet을 Tomcat에 등록
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        tomcat.start();
+    }
+}
